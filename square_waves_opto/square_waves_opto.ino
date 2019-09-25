595042cccb8dd3b72675bc476f1b45e3a273ffc3
Stephen Zhang <x.stephen.zhang@gmail.com>
2019-09-25 10:40:11 -0400

    Update square_waves_opto.ino

--- square_waves_opto/square_waves_opto.ino
+++ square_waves_opto/square_waves_opto.ino
@@ -6,17 +6,17 @@ const float modifier = 1.027;
 const byte ptm_pin = 0;
 const byte opto_pin = 2;
 
-// On for A seconds every B seconds
+// On for A seconds every B seconds (duty cycle = A/B)
 const unsigned long ptm_pulsewidth = 6 * 1000 * modifier; // in micro secs (A)
 const unsigned long ptm_cycletime = 20 * 1000 * modifier; // in micro secs (B)
 
-// On for A seconds every B seconds
+// On for A seconds every B seconds (duty cycle = A/B)
 const unsigned long opto_pulsewidth = 10 * modifier; // in milli secs (A)
 const unsigned long opto_cycletime = 100 * modifier; // in milli secs (A)
 
-// Train is on for A seconds every B seconds
-const unsigned long train_length = 5 * 1000 * modifier; // in milli secs (A)
-const unsigned long train_cycletime = 20 * 1000 * modifier; // in milli secs (B)
+// Train is on for A seconds every B seconds (duty cycle = A/B)
+const unsigned long train_length = 2 * 1000 * modifier; // in milli secs (A)
+const unsigned long train_cycletime = 10 * 1000 * modifier; // in milli secs (B)
 
 
 // Time variables
